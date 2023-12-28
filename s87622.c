d90d793e2fee2dc67854f236bd1609a6b9f1bf80
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2023-12-28 11:56:26 +0100

    refactoring

--- s87622.c
+++ s87622.c
@@ -4,7 +4,7 @@
 #include <openssl/aes.h>
 
 int main(void) {
-    FILE *f_cipher, *f_key, *f_decrypted;
+    FILE *bin_file_cipher, *bin_file_key, *bin_file_decrypted;
     unsigned char key[EVP_MAX_KEY_LENGTH], iv[EVP_MAX_IV_LENGTH];
     const EVP_CIPHER *cipher_type;
     EVP_CIPHER_CTX *ctx;
@@ -12,18 +12,18 @@ int main(void) {
     unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
     int inlen, outlen;
 
-    // Lade das Chiffrat
-    f_cipher = fopen("./bin/s87622-cipher.bin", "rb");
-    if (!f_cipher) {
+    // Lade das Chiffrat aus ./bin/s87622-cipher.bin
+    bin_file_cipher = fopen("./bin/s87622-cipher.bin", "rb");
+    if (!bin_file_cipher) {
         perror("Fehler beim Öffnen der Chiffrat-Datei");
         return 1;
     }
 
-    // Lade den Schlüssel und IV
-    f_key = fopen("./bin/s87622-key1.bin", "rb");
-    if (!f_key) {
+    // Lade den Schlüssel und Intitaliserungs Vektor (IV) aus ./bin/s87622-key1.bin
+    bin_file_key = fopen("./bin/s87622-key1.bin", "rb");
+    if (!bin_file_key) {
         perror("Fehler beim Öffnen der Schlüsseldatei");
-        fclose(f_cipher);
+        fclose(bin_file_cipher);
         return 1;
     }
 
@@ -32,49 +32,49 @@ int main(void) {
     iv_length = EVP_CIPHER_iv_length(cipher_type);
 
     // Lese Schlüssel und IV aus der Datei
-    fread(key, 1, key_length, f_key);
-    fread(iv, 1, iv_length, f_key);
-    fclose(f_key);
+    fread(key, 1, key_length, bin_file_key);
+    fread(iv, 1, iv_length, bin_file_key);
+    fclose(bin_file_key);
 
     // Initialisiere die Entschlüsselung
     ctx = EVP_CIPHER_CTX_new();
     EVP_DecryptInit_ex(ctx, cipher_type, NULL, key, iv);
 
     // Öffne die Ausgabedatei
-    f_decrypted = fopen("decrypted.bin", "wb");
-    if (!f_decrypted) {
+    bin_file_decrypted = fopen("decrypted.bin", "wb");
+    if (!bin_file_decrypted) {
         perror("Fehler beim Öffnen der Ausgabedatei");
-        fclose(f_cipher);
+        fclose(bin_file_cipher);
         EVP_CIPHER_CTX_free(ctx);
         return 1;
     }
 
     // Entschlüsselungsprozess
-    while ((inlen = fread(inbuf, 1, 1024, f_cipher)) > 0) {
+    while ((inlen = fread(inbuf, 1, 1024, bin_file_cipher)) > 0) {
         if (!EVP_DecryptUpdate(ctx, outbuf, &outlen, inbuf, inlen)) {
             // Fehlerbehandlung
             fprintf(stderr, "Fehler bei der Entschlüsselung.\n");
-            fclose(f_cipher);
-            fclose(f_decrypted);
+            fclose(bin_file_cipher);
+            fclose(bin_file_decrypted);
             EVP_CIPHER_CTX_free(ctx);
             return 1;
         }
-        fwrite(outbuf, 1, outlen, f_decrypted);
+        fwrite(outbuf, 1, outlen, bin_file_decrypted);
     }
 
     if (!EVP_DecryptFinal_ex(ctx, outbuf, &outlen)) {
         // Fehlerbehandlung für den finalen Block
         fprintf(stderr, "Fehler bei der finalen Entschlüsselung.\n");
-        fclose(f_cipher);
-        fclose(f_decrypted);
+        fclose(bin_file_cipher);
+        fclose(bin_file_decrypted);
         EVP_CIPHER_CTX_free(ctx);
         return 1;
     }
-    fwrite(outbuf, 1, outlen, f_decrypted);
+    fwrite(outbuf, 1, outlen, bin_file_decrypted);
 
     // Aufräumarbeiten
-    fclose(f_cipher);
-    fclose(f_decrypted);
+    fclose(bin_file_cipher);
+    fclose(bin_file_decrypted);
     EVP_CIPHER_CTX_free(ctx);
 
     printf("Entschlüsselung erfolgreich abgeschlossen.\n");
